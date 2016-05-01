d2b106c6f50912633b62b4b5a5fa32762022f5f5
harsh75 <harshseth75@gmail.com>
2016-05-02 01:03:59 +0530

    The website
    Still in Progress

---
+++ assets/js/panel.js
@@ -0,0 +1,18 @@
+﻿// Green Button Js
+$('#expand-green').on('click', function () {
+    $(this).toggleClass('close-green');
+    $('.text').toggleClass('open-green')
+    .end();
+});
+
+// Red Button JS
+$('#expand-red').on('click', function () {
+    $(this).toggleClass('close-red');
+    $('.text').toggleClass('open-red')
+});
+
+// Blue Button JS
+$('#expand-sky').on('click', function () {
+    $(this).toggleClass('close-sky');
+    $('.text').toggleClass('open-sky');
+});
